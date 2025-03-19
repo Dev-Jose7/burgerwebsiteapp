42bced5381c246f3aaee41d81e966aad5b213a78
Jose Navarro <jfnr398@gmail.com>
2025-03-19 11:35:12 -0500

    chore: se inicializa la estructura del proyecto

---
+++ src/main/java/com/cesdefrontend/burgerwebsite/BurgerwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.cesdefrontend.burgerwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerwebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesdefrontend/burgerwebsite/BurgerwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesdefrontend.burgerwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
